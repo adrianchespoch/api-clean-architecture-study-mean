8d9f622e6fbd22a984915cb485ba8987988ec077
Adrian <alextravc.342@gmail.com>
2023-12-17 17:30:57 -0500

    [ADD]: auth | mongo users repository - create method without Mapper (infra)

---
+++ src/users/infrastructure/repositories/mongo-users.repository.ts
@@ -0,0 +1,33 @@
+import { Nullable } from '@/shared/domain';
+import { User } from '@/users/domain/entities';
+import { UserRepository } from '@/users/domain/repositories';
+import UserModel from './mongo/UserModel';
+
+export class MongoUsersRepository implements UserRepository {
+  findAll(): Promise<User[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  findOne(id: string): Promise<Nullable<User>> {
+    throw new Error('Method not implemented.');
+  }
+
+  findOneByEmail(email: string): Promise<Nullable<User>> {
+    throw new Error('Method not implemented.');
+  }
+
+  async create(user: User): Promise<User> {
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    return newUser as any;
+  }
+
+  update(id: string, user: User): Promise<User> {
+    throw new Error('Method not implemented.');
+  }
+
+  delete(id: string): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+}
