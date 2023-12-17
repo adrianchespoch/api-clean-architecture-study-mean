02fc801361b79b90d7d12e4222ee060f488de1d9
Adrian <alextravc.342@gmail.com>
2023-12-17 17:30:09 -0500

    [ADD]: auth | User Mongo Model (infra)

---
+++ src/users/infrastructure/repositories/mongo/UserModel.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+
+const UserSchema = new Schema(
+  {
+    rol: {
+      type: String,
+      maxlength: 30,
+      required: true,
+      enum: ['client', 'admin'],
+    },
+    name: { type: String, maxlength: 250, required: true },
+    surname: { type: String, maxlength: 250, required: true },
+    email: { type: String, maxlength: 250, required: true, unique: true },
+    password: { type: String, maxlength: 250, required: true },
+    avatar: { type: String, maxlength: 250, required: false },
+    state: { type: Number, default: 1 }, //1 ES ACTIVO 2 ES INACTIVO
+    phone: { type: String, maxlength: 30, required: false },
+    birthday: { type: String, maxlength: 30, required: false },
+    // is_instructor:{type:Number,required:false,default: null},// 1 es instructor
+    profession: { type: String, maxlength: 250, required: false },
+    description: { type: String, required: false },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export default model('User', UserSchema);
