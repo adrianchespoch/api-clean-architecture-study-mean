961f119018191f7c91a9e425bdbaaab3996c4811
Adrian <alextravc.342@gmail.com>
2023-12-17 22:27:53 -0500

    [ADD]: auth | authTokenGenerator injected in UserRegistrator (application)

--- src/auth/application/use-cases/user-registrator.usecase.ts
+++ src/auth/application/use-cases/user-registrator.usecase.ts
@@ -1,13 +1,12 @@
 import { UserAlreadyExistError } from '@/auth/domain/errors';
 import {
-  HandleAuthToken,
+  GenerateAuthToken,
   ProcessPassword,
   RegisterUser,
   UserLike,
   UserToken,
 } from '@/auth/domain/use-cases';
 import { JwtConstants } from '@/shared/application/constants';
-import { InvalidArgumentError } from '@/shared/domain';
 import { User } from '@/users/domain/entities';
 import { UserRepository } from '@/users/domain/repositories';
 
@@ -16,7 +15,7 @@ export class UserRegistrator implements RegisterUser {
   constructor(
     private readonly userRepository: UserRepository,
     private readonly passwordProcessor: ProcessPassword,
-    private readonly authTokenHandler: HandleAuthToken
+    private readonly authTokenGenerator: GenerateAuthToken
   ) {}
 
   async run(userLike: UserLike): Promise<UserToken> {
@@ -35,13 +34,11 @@ export class UserRegistrator implements RegisterUser {
 
     const newUser: User = await this.userRepository.create(user);
 
-    // create usecase for this:
-    const token = await this.authTokenHandler.generateToken(
-      { id: newUser.id },
+    ///* generate JWT
+    const token = await this.authTokenGenerator.run(
+      { id: newUser.id! },
       JwtConstants.duration
     );
-    if (!token)
-      throw new InvalidArgumentError('Something went wrong while creating JWT');
 
     return {
       token,
