658a5722b09031cdeb9b3b86cebf7112cc9ae373
Adrian <alextravc.342@gmail.com>
2023-12-17 19:23:08 -0500

    [ADD]: auth | HandleAuthToken use case (domain)

--- src/auth/domain/use-cases/index.ts
+++ src/auth/domain/use-cases/index.ts
@@ -1,3 +1,4 @@
 export * from './check-admin-privileges.usecase';
+export * from './handle-auth-token.usecase';
 export * from './process-password.usecase';
 export * from './register-user.usecase';
