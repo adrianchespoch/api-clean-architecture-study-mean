b883daa1c41b9e811da0c55c662e98e4e34f2f33
Adrian <alextravc.342@gmail.com>
2023-12-18 16:34:16 -0500

    [ADD]: renew-token endpoint registered in AuthRoutes

--- src/auth/infrastructure/rest/auth.routes.ts
+++ src/auth/infrastructure/rest/auth.routes.ts
@@ -1,16 +1,27 @@
 import { Router } from 'express';
+
+import { AuthMiddleware } from '@/shared/insfrastructure/server/middlewares';
 import { AuthController } from './auth.controller';
 
 export class AuthRoutes {
   ///* DI
-  constructor(private readonly authController: AuthController) {}
+  constructor(
+    private readonly authController: AuthController,
+    private readonly authMiddleware: AuthMiddleware
+  ) {}
 
   get routes(): Router {
     const router = Router();
 
     router.post('/register', this.authController.register);
     router.post('/login', this.authController.login);
-    
+    router.get(
+      '/renew-token',
+      [this.authMiddleware.validateJWT],
+      this.authController.renewJwt
+    );
+
+    // TODO: create admin when app started up & secure this endpoint with AdminPrivilegies
     router.post('/register-admin', this.authController.registerAdmin);
 
     return router;
