f19e09415763cc5d843eb2fd5765785a02e547fc
Adrian <alextravc.342@gmail.com>
2023-12-17 19:10:06 -0500

    [ADD]: auth | hashing password before persist it in UserRegistrator usecase (application)

--- src/auth/application/use-cases/user-registrator.usecase.ts
+++ src/auth/application/use-cases/user-registrator.usecase.ts
@@ -1,19 +1,33 @@
-import { RegisterUser, UserLike, UserToken } from '@/auth/domain/use-cases';
+import { UserAlreadyExistError } from '@/auth/domain/errors';
+import {
+  ProcessPassword,
+  RegisterUser,
+  UserLike,
+  UserToken,
+} from '@/auth/domain/use-cases';
 import { User } from '@/users/domain/entities';
 import { UserRepository } from '@/users/domain/repositories';
 
-
 export class UserRegistrator implements RegisterUser {
-
   ///* DI
-  constructor(private readonly userRepository: UserRepository) {}
-
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly passwordProcessor: ProcessPassword
+  ) {}
 
   async run(userLike: UserLike): Promise<UserToken> {
+    const userSaved = await this.userRepository.findOneByEmail(userLike.email);
+    if (userSaved) throw new UserAlreadyExistError('Email already registered');
+
+    ///* hash password & setup user before to persist
+    userLike.password = this.passwordProcessor.hash(userLike.password);
+
     userLike.state = true;
 
     ///* create user to register DomainEvents
     const user = User.create(userLike);
+    /// usecase (app) is who actually Publish DomainEvents
+    // await this.eventBus.publish(course.pullDomainEvents());
 
     const newUser: User = await this.userRepository.create(user);
 
@@ -22,5 +36,4 @@ export class UserRegistrator implements RegisterUser {
       user: newUser,
     };
   }
-
 }
